5d04ec3cdf3760314d9a09592a6a8fcdeaa79dd6
Vamsi Krishna Ponnada <VamsiKrishna.Ponnada@iiitb.org>
2020-05-18 02:09:05 +0530

    Initial commit: A books web project

--- src/main/java/org/myBooks/controller/AuthenticateController.java
+++ src/main/java/org/myBooks/controller/AuthenticateController.java
@@ -1,21 +1,32 @@
 package org.myBooks.controller;
 
 import org.myBooks.services.AuthenticationService;
+import org.myBooks.util.Constants;
 
 import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import java.net.URI;
 
 @Path("/Authenticate")
 public class AuthenticateController {
+    @POST
+    public Response authenticate(
+            @FormParam("emailN") String userId,
+            @FormParam("passwordN") String pwd
+    ) throws java.net.URISyntaxException
+    {
+        boolean status = new AuthenticationService().authenticate(userId, pwd);
+        String url = Constants.HOME;
+        url += status? "index.html": "Login.html";
 
-    @GET
-//    @Path("/authenticate")
-    public Response authenticate() {
-        boolean status = new AuthenticationService().authenticate("vamceep", "abs");
-        String msg = status?"success":"invalid";
-        return Response.ok(msg).build();
+        if(status) {
+             url +="?userName="+userId;
+            return Response.seeOther(new URI(url)).build();
+        }
+        return Response.seeOther(new URI(url)).build();
+
     }
 
 }
