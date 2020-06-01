7c4ad7e2aa455206b3cffe38ab462052bced45a8
nidhibudhraja <nidhi.budhraja96@gmail.com>
2020-06-01 18:08:38 +0530

    changed config to match with working branch

--- src/main/java/org/myBooks/util/Constants.java
+++ src/main/java/org/myBooks/util/Constants.java
@@ -1,5 +1,5 @@
 package org.myBooks.util;
 
 public class Constants {
-    public static final String HOME = "http://localhost:8055/Books_war/";
+    public static final String HOME = "http://localhost:8088/Books_war/";
 }
